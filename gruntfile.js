610de3e21df1e9093ddf6867bd08227d24485f29
Koffi Anderson Koffi <kandersonko@gmail.com>
2016-10-25 15:45:20 -0700

    grunt, grunt-contrib-uglify, and grunt-contrib-cssmin added to package.json via npm.

---
+++ gruntfile.js
@@ -0,0 +1,36 @@
+/*
+  Grunt file for minifying the javascript and css sources for a faster loading time.
+*/
+
+module.exports = function(grunt) {
+  debug = false;
+
+  
+
+  // Different configuration for the project
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      build: {
+        src: 'src/js/*.js',
+        dest: 'build/js/app.min.js'
+      }
+    },
+
+    cssmin: {
+      build: {
+        files: {
+          'build/css/app.min.css' : ['src/css/*.css']
+        }
+      }
+    }
+  });
+
+  // Load tasks from grunt plugin install via npm
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task.
+  grunt.registerTask('default', ["uglify", "cssmin"]);
+}
