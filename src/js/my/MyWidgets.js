32d1465a7dd8929f7dd6c2feaa59e91ee1f32dfd
Koffi Anderson Koffi <kandersonko@gmail.com>
2016-11-15 13:08:25 -0800

    Search restricted to Idaho counties

--- src/js/my/MyWidgets.js
+++ src/js/my/MyWidgets.js
@@ -4,18 +4,40 @@
 define([
     "dojo/_base/declare",
     "esri/widgets/Search",
+    "esri/layers/FeatureLayer",
+    "esri/symbols/PictureMarkerSymbol",
     "dojo/domReady!"
   ],
-  function (declare, Search) {
+  function (declare, Search, FeatureLayer, PictureMarkerSymbol) {
     return declare(null, {
       searchWidget: null,
       constructor: function (view) {
         this.searchWidget = new Search({
           view: view,
-          allPlaceholder: "Land Management or cover"
+
+          sources: [
+            {
+              featureLayer: new FeatureLayer({
+                url: "https://gis-sandbox.northwestknowledge.net/arcgis/rest/services/idaho_rangeland_atlas/ira_2014_county_boundaries/MapServer/0",
+                popupTemplate: {
+                  title: "<h3>{NAME}</h3>",
+                  overwriteActions: true
+                }
+              }),
+              placeholder: "County",
+              searchFields: ["NAME"],
+              suggestionTemplate: "{Name}",
+              exactMatch: false,
+              outFields: ["*"],
+              name: "Counties",
+              zoomScale: 500000
+            }
+          ]
         });
         this.searchWidget.startup();
       },
-      search: function() { return this.searchWidget; }
+      search: function () {
+        return this.searchWidget;
+      }
     });
   });
