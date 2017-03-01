924d6b8024340cb568c80fed578f48c177b97fed
Koffi Anderson Koffi <kandersonko@gmail.com>
2017-03-01 15:56:02 -0800

    save map as image function created

--- src/js/main.js
+++ src/js/main.js
@@ -28,11 +28,22 @@ $(document).ready(function(){
 
 function exportPDF(){
   var filename = document.getElementsByClassName('county-title')[0].textContent.toLowerCase() + " result.pdf";
-  var map = document.getElementById("mapCanvas");
-  console.log("map: ", map);
   var doc = new jsPDF('p', 'pt');
   var elem = document.getElementsByClassName('table-result')[0];
   var res = doc.autoTableHtmlToJson(elem);
   doc.autoTable(res.columns, res.data);
   doc.save(filename);
+}
+
+function exportPagePDF(){
+  html2canvas($("#mapCanvas"), {
+    onrendered: function(canvas) {
+      var imgData = canvas.toDataURL(
+        'image/jpeg');
+      var doc = new jsPDF('p', 'mm');
+      doc.addImage(imgData, 'JPEG', 15, 40, 180, 180);
+      doc.save('sample-file.pdf');
+    }
+  });
+
 }
