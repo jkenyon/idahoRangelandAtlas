086db068d97e859e6e24e9301b5c2be0e6de8aa0
Koffi Anderson Koffi <kandersonko@gmail.com>
2017-03-08 15:00:15 -0800

    fix some issues with printing the map

--- src/js/main.js
+++ src/js/main.js
@@ -29,23 +29,6 @@ $(document)
 
   });
 
-// function exportPDF() {   var countyName =
-// document.getElementsByClassName('county-title')[0].textContent;   var
-// filename = countyName.toLowerCase() + " result.pdf";   var doc = new
-// jsPDF('p', 'mm');   var elem =
-// document.getElementsByClassName('table-result')[0];   var res =
-// doc.autoTableHtmlToJson(elem);   // hide map widgets
-// $('.esri-ui-top-left.esri-ui-corner,   //
-// .esri-ui-top-right.esri-ui-corner').hide();   html2canvas($("#mapCanvas"),
-// {allowTaint: true})     .then(function (canvas) {       canvas.allowTaint =
-// true;       canvas.crossOrigin = true;       var img = new Image;
-// img.crossOrigin = "anonymous";       img = canvas.toDataURL('image/jpeg');
-// doc.text(countyName.toUpperCase(), 75, 30);       doc.addImage(img, 'jpeg',
-// 15, 40, 180, 180);       doc.addPage(); doc.autoTable(res.columns, res.data);
-//       doc.save(filename);     }) .then(function () {       //
-// $('.esri-ui-top-left.esri-ui-corner,       //
-// .esri-ui-top-right.esri-ui-corner').show();     }); }
-
 function exportPDF() {
   var countyName = document.getElementsByClassName('county-title')[0].textContent;
   var filename = countyName.toLowerCase() + " result.pdf";
@@ -53,7 +36,7 @@ function exportPDF() {
   var elem = document.getElementsByClassName('table-result')[0];
   var res = doc.autoTableHtmlToJson(elem);
   // hide map widgets
-  $('.esri-ui-top-left.esri-ui-corner .esri-ui-top-right').hide();
+  $('.esri-ui-inner-container, .esri-ui-corner-container').hide();
   html2canvas($("#mapCanvas"), {
     useCORS: true
   })
@@ -75,46 +58,7 @@ function exportPDF() {
       }
     });
     doc.save(filename);
-    $('.esri-ui-top-left, .esri-ui-top-right').show();
+    $('.esri-ui-inner-container, .esri-ui-corner-container').show();
   });
-
-}
-
-// function exportPagePDF() {
-//   html2canvas($("#mapCanvas"), {
-//     onrendered: function (canvas) {
-//       var imgData = canvas.toDataURL('image/jpeg');
-//       var doc = new jsPDF('p', 'mm');
-//       doc.addImage(imgData, 'JPEG', 15, 40, 180, 180);
-//       doc.save('sample-file.pdf');
-//     }
-//   });
-
-// }
-
-function exportPagePDF() {
-  var countyName = document.getElementsByClassName('county-title')[0].textContent;
-  var filename = countyName.toLowerCase() + " result.pdf";
-  var doc = new jsPDF('p', 'mm');
-  var elem = document.getElementsByClassName('table-result')[0];
-  var res = doc.autoTableHtmlToJson(elem);
-  html2canvas($("#mapCanvas"), {
-    onrendered: function (canvas) {
-      canvas.allowTaint = true;
-      var img = new Image;
-      imgData.crossOrigin = "anonymous";
-      imgData.src = 'image/jpeg';
-      var imgData = canvas.drawImage(img);
-      doc.text(countyName.toUpperCase(), 100, 10);
-      doc.addImage(imgData, 'JPEG', 15, 40, 180, 180);
-      doc.addPage();
-    },
-    allowTaint: true,
-    useCORS: true
-  })
-    .then(function () {
-      doc.autoTable(res.columns, res.data);
-      doc.save(filename);
-    });
 
 }
